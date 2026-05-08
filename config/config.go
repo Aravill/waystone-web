b0a0c561f75bc825c61352864205005c3f0ef093
aravill <mozikmichal@gmail.com>
2026-05-09 00:02:10 +0200

    Updates initial seed data and adds a new user

--- config/config.go
+++ config/config.go
@@ -21,15 +21,34 @@ var InitialCampaigns = []models.Campaign{
 		DM:          "550e8400-e29b-41d4-a716-446655440000",
 		SignUpsOpen: true,
 	},
+	{
+		ID:          "ea213cb4-9ab9-49ff-a29f-3fafb7b4799c",
+		Title:       "Embittered Arcanist",
+		Status:      models.Pitch,
+		Summary:     "Figure out who turned the Archamge into a sheep",
+		Description: "Lorem ipsum dolor sit amet something something bla bla bla",
+		Players:     []string{"550e8400-e29b-41d4-a716-446655440000"},
+		DM:          "7609bdaf-cc43-4131-b188-098aa07ba6dc",
+		SignUpsOpen: true,
+	},
 }
 
 var InitialUsers = []models.User{
 	{
 		ID:        "550e8400-e29b-41d4-a716-446655440000",
 		Email:     "mozikmichal@gmail.com",
-		Name:      "Admin",
-		Nickname:  "Michi",
-		Roles:     []string{"admin"},
+		Name:      "Michal Mozik",
+		Nickname:  "Aravill",
+		Roles:     []string{"admin", "user"},
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	},
+	{
+		ID:        "7609bdaf-cc43-4131-b188-098aa07ba6dc",
+		Email:     "test.user@gmail.com",
+		Name:      "Test Testington",
+		Nickname:  "Testy",
+		Roles:     []string{"user"},
 		CreatedAt: time.Now(),
 		UpdatedAt: time.Now(),
 	},
